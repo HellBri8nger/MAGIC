f9e39ddc976afbb2039a0aff3470e982c072decc
Lupin <theybetrayedme1@gmail.com>
2024-06-27 11:51:20 +0530

    feat: fix: fixed a bug that was causing for multiple even listeners to be added

--- src/games/minecraft/src/main/java/com/hellbri8nger/magic/Magic.java
+++ src/games/minecraft/src/main/java/com/hellbri8nger/magic/Magic.java
@@ -9,35 +9,45 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.net.URISyntaxException;
 
 public class Magic implements ModInitializer {
 	public static final String MOD_ID = "magic";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private Socket socket;
 
 	@Override
 	public void onInitialize() {
-		// Runs as soon as minecraft is in a mod-load-ready state
-
+		// Runs as soon as Minecraft is in a mod-load-ready state
 		ServerLifecycleEvents.SERVER_STARTED.register(this::sendMessageToChat);
-
 	}
 
-	public void sendMessageToChat(MinecraftServer server){
+	public void sendMessageToChat(MinecraftServer server) {
 		try {
-			Socket socket = IO.socket("http://localhost:3001");
+			socket = IO.socket("http://localhost:3001");
 
 			socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 				@Override
 				public void call(Object... args) {
 					LOGGER.info("Connected to the server");
+					server.getPlayerManager().broadcast(Text.literal("Connected to the server"), false);
+				}
+			});
 
-					socket.on("magic", new Emitter.Listener() {
-						@Override
-						public void call(Object... args) {
-							server.getPlayerManager().broadcast(Text.literal((String) args[0]), false);
-						}
-					});
+			socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+				@Override
+				public void call(Object... args) {
+					LOGGER.info("Disconnected from the server");
+					server.getPlayerManager().broadcast(Text.literal("Disconnected from the server"), false);
+					socket.off("magic");
+				}
+			});
+
+			socket.on("magic", new Emitter.Listener() {
+				@Override
+				public void call(Object... args) {
+					server.getPlayerManager().broadcast(Text.literal((String) args[0]), false);
 				}
 			});
 
@@ -48,5 +58,4 @@ public class Magic implements ModInitializer {
 			throw new RuntimeException(e);
 		}
 	}
-
 }
